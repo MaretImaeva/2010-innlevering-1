7e0b6ed6c421d2d92a8fd8f25c132fd0cef3c051
Mariat <maretri@uio.no>
2025-10-01 14:55:35 +0200

    Update Main.py

--- innlevering 2/Main.py
+++ innlevering 2/Main.py
@@ -1,1 +1,9 @@
+filnavn = input("Vennligst legg ved din fil:\n")
 
+with open(filnavn+"_merge.out", "w") as file:
+    # Write content to the file
+    file.write("This is the first line.\n")
+    file.write("This is the second line.")
+
+print("Content written to my_file.txt (overwriting if it existed).")
+  
