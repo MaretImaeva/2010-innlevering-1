4b8f2c72821a4599ca8f125f16c0996c248af371
Esmaaa <esmabo@uio.no>
2025-11-05 22:37:38 +0100

    Refactor DFS and component size functions

--- innlevering 3/Mariat/oppgave2.py
+++ innlevering 3/Mariat/oppgave2.py
@@ -1,6 +1,6 @@
 from graf1000 import alt_dict
 
-def DFS(G, s, visited):  # beholder signaturen, ignorerer G
+def DFS(s, visited):  
     resultat = []
     stack = [s]
 
@@ -9,18 +9,15 @@ def DFS(G, s, visited):  # beholder signaturen, ignorerer G
         if u not in visited:
             resultat.append(u)
             visited.add(u)
-            # naboer ligger i alt_dict[u]["kant"] som (nabo_id, tittel, filmID, vekt)
             for v, film_tittel, film_id, rating in alt_dict.get(u, {}).get("kant", []):
                 if v not in visited:
                     stack.append(v)
     return resultat
 
 # Finner størrelsen på komponentene
-def kompSize(G):  # beholder signaturen, ignorerer G
+def kompSize(): 
     visited = set()
     size = []
-
-    # bygg V fra nøkler + naboer i alt_dict
     V = set(alt_dict.keys())
     for entry in alt_dict.values():
         for v_id, film_tittel, film_id, rating in entry.get("kant", []):
@@ -28,19 +25,19 @@ def kompSize(G):  # beholder signaturen, ignorerer G
 
     for v in V:
         if v not in visited:
-            comp = DFS(G, v, visited)
+            comp = DFS( v, visited)
             size.append(len(comp))
     return size
 
-def skrivStørrelser(G):  # beholder signaturen, ignorerer G
-    size = kompSize(G)
+def skrivStørrelser():  
+    size = kompSize()
     freq = {}
     for i in size:
         freq[i] = freq.get(i, 0) + 1
     for i in freq:
         print("There are", freq[i], "components of size", i)
 
-# kall med en dummy (G brukes ikke lenger)
-skrivStørrelser(None)
+skrivStørrelser()
+
 
 
